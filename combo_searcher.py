c711010cbad7649ffb3b0c55ebea27f33cd4abf3
solin020 <solin020@umn.edu>
2021-08-04 01:34:59 -0500

    This version handles associativity, commutivity, and unary operators

--- combo_searcher.py
+++ combo_searcher.py
@@ -1,142 +1,173 @@
-from itertools import combinations, product
+from itertools import product
 import random
 
 
-_minimum_increase = -1
 
+class Expression:
+    def __init__(self, op, *children):
+        self.op = op
+        self.children = children
 
-def compose(names, op):
-    r = names[0].expr + op + names[1].expr
-    for n in names[2:]:
-        r = f'({r}){op}{n.expr}'
-    return r
+    def __hash__(self):
+        if not hasattr(self, '_hash'):
+            leaf_list = list(self.leaves)
+            leaf_list.sort()
+            self._hash = hash(tuple(leaf_list))
+        return self._hash
 
-def partitions(n, I=1, start=True):
-    if not start:
-        yield (n,)
-        for i in range(I, n//2 + 1):
-            for p in partitions(n-i, i, False):
-                yield (i,) + p
-    elif start:
-        for i in range(I, n//2 + 1):
-            for p in partitions(n-i, i, False):
-                yieldval = {}
-                for j in ( (i,) + p):
-                    yieldval[j] = yieldval.get(j, 0) + 1
-                yield yieldval
-
-
-
-class Node:
-    def __init__(self, name, scorer):
-        self.names = set([name])
-        self.expr = name
-        self.scorer = scorer
-        self.components = self
+    def __eq__(self, other):
+        if type(other) == Leaf:
+            return False
+        elif self.op == other.op:
+            if self.op.associative:
+                return self.associative_set == other.associative_set
+            elif self.op.commutative:
+                return self.child_set == other.child_set
+            elif self.op.unary:
+                return self.children[0] == other.children[0]
+            else:
+                return [f==g for f,g in zip(self.children, other.children)]
+        return False
 
     @property
-    def score(self):
-        if not hasattr(self, '_score'):
-            self._score = self.scorer(self.expr)
-        return self._score
-
-    def __repr__(self):
-        return f'{self.expr}'
-
-
-class UnionSet(Node):
-    def __init__(self, names, op, scorer):
-        self.names = set().union(*(n.names for n in names))
-        self.components = names
-        self.op = op
-        self.scorer = scorer
-
+    def associative_set(self):
+        if not hasattr(self, '_associative_set'):
+            retval = set()
+            for child in self.children:
+                if child.op == self.op:
+                    retval.update(child.associative_set)
+                else:
+                    retval.add(child)
+            self._associative_set = retval
+        return self._associative_set
 
     @property
-    def expr(self):
-        retval = compose(self.components, self.op)
-        return f'({retval})'
+    def child_set(self):
+        if not hasattr(self, '_child_set'):
+            self._child_set = set(self.children)
+        return self._child_set
 
 
+    def __str__(self):
+        if self.op.unary:
+            return f'{self.op.symbol}({self.children[0]})'
+        else:
+            return self.op.symbol.join(f'({child})' for child in self.children)
+    @property
+    def leaves(self):
+        if not hasattr(self, '_leaves'):
+            self._leaves = set().union(*[child.leaves for child in self.children])
+        return self._leaves
 
+class Op:
+    def __init__(self, symbol = "", associative=False, commutative=False, unary=False):
+        self.symbol = symbol
+        self.associative = associative
+        self.commutative = commutative
+        self.unary = unary
 
-class TierSearcher:
-    def __init__(self, names, op, order, scorer):
-        self.tiers = {i:{'contents': [], 'union': {1:set()}} for i in range(1, order + 1)}
-        self.op = op
-        self.scorer = scorer
-        self.order = order
-        self.tiers[1]['contents'] = names
-        self.tiers[1]['union'][1] = self.tiers[1]['union'][1].union(names)
-        for o in range(2, order + 1):
-            for combo in combinations((n for n in self.tiers[o-1]['union'][1]), o):
-                insert_val = UnionSet(combo, op, scorer)
-                if all(insert_val.score > n.score + _minimum_increase for n in insert_val.components):
-                    self.tiers[o]['contents'].append(insert_val)
-                    self.tiers[o]['union'][1] = self.tiers[o]['union'][1].union(combo)
+andop = Op('&', associative = True)
+orop = Op('|', associative = True)
+xorop = Op('^', commutative = True)
+notop = Op('~', unary=True)
+leafop = Op('')
 
-    def insert(self, exprs, size):
-        order_new_union = {size:set()}
-        for o in range(size + 1, self.order + 1):
-            inssize = set()
-            for p in partitions(o):
-                if size in p:
-                    for sizechoose in range(1, p[size] + 1):
-                        oldchoose = p[size] - sizechoose
-                        for combo in product(
-                            combinations(exprs, sizechoose),
-                            combinations(self.tiers[o -1]['union'].get(size, []), oldchoose),
-                            *(combinations(self.tiers[o -1]['union'].get(s, []), pp) for s, pp in p.items()
-                              if s!= size)
-                        ):
-                            news = combo[0]
-                            combo = sum(combo, ())
-                            total_size = sum(len(c.names) for c in combo)
-                            union_size = len(combo[0].names.union(*(c1.names for c1 in combo[1:])))
-                            if union_size == total_size:
-                                insert_val = UnionSet(combo, self.op, self.scorer)
-                                if all(insert_val.score > v.score + _minimum_increase for v in insert_val.components):
-                                    self.tiers[o]['contents'].append(insert_val)
-                                    inssize = inssize.union(news)
-            order_new_union[o] = inssize
-        for o, v in order_new_union.items():
-            self.tiers[o]['union'][size] = self.tiers[o]['union'].get(size, set()).union(v)
+class Leaf(Expression):
+    op = leafop
+    def __init__(self, name):
+        self.name = name
 
-    def __repr__(self):
-        return repr(self.tiers)
+    def __eq__(self, other):
+        if type(other) == Expression:
+            return False
+        return self.name == other.name
 
+    def __str__(self):
+        return self.name
 
+    def __hash__(self):
+        return hash(self.name)
 
-def rs(n):
+    @property
+    def leaves(self):
+        return {self.name}
+    
+
+def test_scoring_function(e):
     return random.random()
 
-def get_best_ensembles(score_method,
-                       names = ['biomedicus', 'quick_umls', 'ctakes', 'clamp', 'metamap'],
-                       operators = ['&', '|', '^'],
-                       order = None, minimum_increase=0):
-    names = [Node(name, score_method) for name in names]
-    operators = set(operators)
-    if order is None:
-        order = len(names)
-    global _minimum_increase
-    _minimum_increase = minimum_increase
-    opdict = {o: TierSearcher(names, o, order, score_method) for o in operators}
-    for size in range(2, order):
-        for others in combinations(operators, len(operators) - 1):
-            original = opdict[next(iter(operators.difference(others)))]
-            for o in others:
-                o = opdict[o]
-                original.insert(o.tiers[size]['contents'], size)
-    retval =  [(n.expr, n.score)
-        for o in opdict.values()
-        for tier in list(o.tiers.values())[1:]
-        for n in tier['contents']
-    ] + [(n.expr, n.score)
-        for o in list(opdict.values())[0:1]
-        for tier in list(o.tiers.values())[0:1]
-        for n in tier['contents']
-    ]
+#used_binops is the binary operators being combined. used_unops is the unary_operators being combined
+#used_leaves is the name of all the sources for the ensembles
+#minimum_improvement is how much improvement needs to occur each step for a subensemble to be added to the search list
+#no_overlap, if True, forbids the same source being used twice in an expression
+def get_best_ensembles(score_method=test_scoring_function, 
+           names=['metamap', 'ctakes', 'clamp', 'biomedicus', 'quick_umls'], 
+           used_binops=[andop, orop, xorop], 
+           used_unops = [notop], 
+           minimum_increase=0.1, 
+           no_overlap=True):
+    tiers = {}
+    minimum_improvement = minimum_increase
+    used_leaves = names
+    def scoref(e):
+        return score_method(str(e))
+    tiers[1] = []
+    leaf_no = len(used_leaves)
+    for leaf in used_leaves:
+        leaf = Leaf(leaf)
+        score = scoref(leaf)
+        leaf.score = score
+        tiers[1].append(leaf)
+    for outsize in range(1, leaf_no+1):
+        tiers[outsize] = tiers.get(outsize, [])
+        for insize in range(1, outsize):
+            lsize = insize
+            rsize = outsize - insize
+            for l, r in product(tiers[lsize], tiers[rsize]):
+                if no_overlap:
+                    if not l.leaves.isdisjoint(r.leaves): 
+                        continue
+                for b in used_binops:
+                    e = Expression(b, l, r)
+                    if e in tiers[outsize]: 
+                        continue
+                    lscore = l.score
+                    rscore = r.score
+                    escore = scoref(e)
+                    e.score = escore
+                    compscore = max(lscore, rscore)
+                    if escore > compscore + minimum_improvement:
+                        print(e, e.score)
+                        tiers[outsize].append(e)
+        appendend = []
+        for negator in tiers[outsize]:
+            for unop in used_unops:
+                e = Expression(unop, negator)
+                escore = scoref(e)
+                e.score = escore
+                if e.score > leaf.score + minimum_improvement:
+                    print(e, e.score)
+                    appendend.append(e)
+        tiers[outsize] += appendend 
+    retval = []
+    for v in tiers.values():
+        for vv in v:
+            retval.append((str(vv), vv.score))
     retval.sort(key=lambda x: x[1], reverse=True)
     return retval
 
 
+
+##associative
+aa = Expression(orop, Expression(orop, Leaf('a'), Leaf('b')), Leaf('c'))
+bb = Expression(orop, Expression(orop, Leaf('b'), Leaf('c')), Leaf('a'))
+print('associative should be true', aa == bb)
+
+
+aa = Expression(xorop, Expression(xorop, Leaf('a'), Leaf('b')), Leaf('c'))
+bb = Expression(xorop, Expression(xorop, Leaf('b'), Leaf('c')), Leaf('a'))
+print('only commutative should not be true', aa==bb)
+
+aa = Expression(xorop, Expression(xorop, Leaf('a'), Leaf('b')), Leaf('c'))
+bb = Expression(xorop, Expression(xorop, Leaf('b'), Leaf('a')), Leaf('c'))
+print('commutative should be true', aa==bb)
