b8842bb2ca3399ef059a9a0b60ab84d9ac82ae50
solin020 <solin020@umn.edu>
2021-08-16 16:20:56 -0500

    Update combo_searcher.py

--- combo_searcher.py
+++ combo_searcher.py
@@ -16,7 +16,7 @@ class Expression:
         return self._hash
 
     def __eq__(self, other):
-        if type(other) == Leaf:
+        if type(other) != type(self):
             return False
         elif self.op == other.op:
             if self.op.associative:
@@ -27,7 +27,31 @@ class Expression:
                 return self.children[0] == other.children[0]
             else:
                 return [f==g for f,g in zip(self.children, other.children)]
-        return False
+        return self.demorgans(self, other)
+
+    @staticmethod
+    def demorgans(l, r):
+        if l.op == notop and l.children[0].op in (andop, orop):
+            n = l
+            bi = r
+        elif r.op == notop and r.children[0].op in (andop, orop):
+            n = r
+            bi = l
+        else:
+            return False
+        nop = n.children[0].op
+        biop = bi.op
+        if not (nop == andop and biop == orop or nop == orop and biop == andop):
+            return False
+        a = n.children[0].children[0]
+        b = n.children[0].children[1]
+        c = bi.children[0]
+        d = bi.children[1]
+        if not (c.op == notop and d.op == notop):
+            return False
+        return a == c.children[0] and b == d.children[0]
+
+
 
     @property
     def associative_set(self):
@@ -66,6 +90,9 @@ class Op:
         self.commutative = commutative
         self.unary = unary
 
+    def __str__(self):
+        return self.symbol
+
 andop = Op('&', associative = True)
 orop = Op('|', associative = True)
 xorop = Op('^', commutative = True)
@@ -78,7 +105,7 @@ class Leaf(Expression):
         self.name = name
 
     def __eq__(self, other):
-        if type(other) == Expression:
+        if type(other) != type(self):
             return False
         return self.name == other.name
 
@@ -170,4 +197,47 @@ print('only commutative should not be true', aa==bb)
 
 aa = Expression(xorop, Expression(xorop, Leaf('a'), Leaf('b')), Leaf('c'))
 bb = Expression(xorop, Expression(xorop, Leaf('b'), Leaf('a')), Leaf('c'))
+
 print('commutative should be true', aa==bb)
+
+aa = Expression(notop, Expression(andop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans 1 should be true', aa==bb)
+
+
+aa = Expression(notop, Expression(orop, Leaf('a'), Leaf('b')))
+bb = Expression(andop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans 2 should be true', aa==bb)
+
+aa = Expression(notop, Expression(orop, Leaf('a'), Leaf('b')))
+bb = Expression(andop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans 3 should be true', bb==aa)
+
+aa = Expression(notop, Expression(andop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans 4 should be true', bb==aa)
+
+
+aa = Expression(notop, Expression(andop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(andop, Leaf('b')))
+print('demorgans difference 1 should not be true', aa==bb)
+
+
+aa = Expression(notop, Expression(andop, Leaf('a'), Leaf('b')))
+bb = Expression(andop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans difference 2 should not be true', aa==bb)
+
+
+aa = Expression(notop, Expression(orop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(notop, Leaf('b')))
+print('demorgans difference 3 should not be true', aa==bb)
+
+
+aa = Expression(notop, Expression(orop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(andop, Leaf('b')))
+print('demorgans difference 4 should not be true', aa==bb)
+
+
+aa = Expression(notop, Expression(andop, Leaf('a'), Leaf('b')))
+bb = Expression(orop, Expression(notop, Leaf('a')), Expression(notop, Leaf('c')))
+print('demorgans difference 5 should not be true', aa==bb)
